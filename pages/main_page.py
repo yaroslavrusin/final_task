9209d89dc711122921ee11b789a759a78f014abd
Yaroslav <yaroslav-rusin@mail.ru>
2023-04-13 16:44:18 +0300

    add class MainPage and new test case

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,11 @@
-class BasePage():
-    def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
 
-    def open(self):
-        self.browser.get(self.url)
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
+
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
