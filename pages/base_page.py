23b9074c7af3816e47d139577ec0df3b7de4b795
Yaroslav <yaroslav-rusin@mail.ru>
2023-04-14 15:05:49 +0300

    transferring text with logging to the BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoAlertPresentException
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -11,6 +12,10 @@ class BasePage():
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link.click()
 
     def open(self):
         self.browser.get(self.url)
@@ -37,6 +42,10 @@ class BasePage():
         except TimeoutException:
             return False
         return True
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), \
+            "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
